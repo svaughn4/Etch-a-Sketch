58268ef23b9ec453b5ccf1046c8c52c7d3a3cd9e
Syd <svaughn4@stanford.edu>
2022-07-26 13:42:16 -0700

    Add error input prompt

--- javascript.js
+++ javascript.js
@@ -1,20 +1,36 @@
-// create the parent node
-const container = document.querySelector('.container');
+let size = 16; 
 
-// loop through to create 16 X 16 grid
-for(let i = 1; i <= 16; i++) { 
-    // create mini div for each row
-    const mini = document.createElement('div');  
-    mini.classList.add('row');
-    for (let j = 1; j <= 16; j++) { 
-        const child = document.createElement('div');
-        child.classList.add('cell'); 
-        child.style.borderColor='red';
-        //child.textContent= 'hey'; 
-        mini.appendChild(child);
+// adds popup to ask for user's input for grid 
+document.getElementById('myBtn').addEventListener('click', () => { 
+    let input = prompt("Enter the number of squares per side you want"); 
+    size = parseInt(input);
+    console.log(size);
+    if (input == null || input > 100 || input == NaN) { 
+        alert("You must enter a valid number equal to or less than 100");
+    } else { 
+        makeGrid(size);
     }
-    container.appendChild(mini);
-} 
+});  
+
+// create the parent node
+const container = document.querySelector('.container'); 
+
+// create the grid based on input
+function makeGrid(size) {
+    for(let i = 1; i <= size; i++) { 
+        // create mini div for each row
+        const mini = document.createElement('div');  
+        mini.classList.add('row');
+        for (let j = 1; j <= size; j++) { 
+            const child = document.createElement('div');
+            child.classList.add('cell'); 
+            child.style.borderColor='red';
+            //child.textContent= 'hey'; 
+            mini.appendChild(child);
+        }
+        container.appendChild(mini);
+    }  
+}
 
 // add hover event listener to each cell
 document.querySelectorAll('.cell').forEach(div => {
@@ -25,14 +41,3 @@ document.querySelectorAll('.cell').forEach(div => {
 })
 
 
-// adds popup to ask for user's input for grid 
-document.getElementById('myBtn').addEventListener('click', () => { 
-    let input = prompt("Enter the number of squares per side you want");
-    if (input != null) { 
-        gridSize();
-    }
-}); 
-
-// asks for number of squares per side for the new grid
-function gridSize() { 
-}
