61e74b313b0087d7e9c0dada7bf56b3b00bee6f0
Syd <svaughn4@stanford.edu>
2022-07-28 08:58:07 -0700

    Calculate side lengths of cells based on input

--- javascript.js
+++ javascript.js
@@ -1,15 +1,20 @@
-let size;  
+// initiate size (number of grid cells per row) and side length of each cell
+let size, length;
+
+
 // create the parent node
 const container = document.querySelector('.container'); 
 
 // adds popup to ask for user's input for grid 
 document.getElementById('myBtn').addEventListener('click', () => { 
     let input = prompt("Enter the number of squares per side you want"); 
-    size = parseInt(input);
-    console.log(size);
-    if (input == null || input > 100 || input == NaN) { 
+    size = parseFloat(input); 
+    //console.log("size: ", size);
+    if (input == null || input > 100 || input == NaN || input == 0) { 
         alert("You must enter a valid number equal to or less than 100");
     } else { 
+        length = parseFloat(((1000 / (size * 2)) ** 0.5).toFixed(2)); 
+        //console.log("length: ", length);
         if(container.firstChild == null) { 
             makeGrid(size);
         } else {  
@@ -19,31 +24,29 @@ document.getElementById('myBtn').addEventListener('click', () => {
     }
 });  
 
-// create the grid based on input
+// make a grid cell based on the side length 
+function makeCell () { 
+    let div = document.createElement('div');
+    div.classList.add('cell'); 
+    div.style.width=(`${length}px`);  
+    div.style.height=(`${length}px`);
+    div.addEventListener('mouseover', () => { 
+        div.setAttribute('style', 'background-color: black')
+    }) 
+    return div;
+}
+
+
+// create the grid based on input number of grid cells
 function makeGrid(size) {
-    for(let i = 1; i <= size; i++) { 
-        // create mini div for each row
-        const mini = document.createElement('div');  
-        mini.classList.add('row');
-        for (let j = 1; j <= size; j++) { 
-            let child = document.createElement('div');
-            child.classList.add('cell');  
-
-            // add border to each cell
-            child.style.border='1px solid black';  
-
-            // add hover event listener to each cell
-            child.addEventListener('mouseover', () => {
-                child.setAttribute('style', 'background-color: black')
-            })
-            //child.textContent= 'hey'; 
-            mini.appendChild(child);
+    for(let i = 0; i < size; i++) { 
+        for (let j = 0; j < size; j++) { 
+            container.appendChild(makeCell());
         }
-        container.appendChild(mini);
     }  
 }
 
-// clears away current grid for the new requested grid
+// clears away current grid
 function cleared() { 
     while(container.firstChild) { 
         container.removeChild(container.firstChild);
